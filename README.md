1f24d238fba5368d2cafd29baa9a8b68fc612458
Nebula <40148908+nebula-it@users.noreply.github.com>
2024-12-08 12:36:05 -0800

    feat: Keep track of submitted PR and versions

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 # winget-pkgs-automation
 Repo runs Github Action to deploy new package version automagically on Winget
+
+# Submit a new Package
+```
+wingetcreate new --out $env:TEMP -t <github Token> https://github.com/cilium/cilium-cli/releases/download/v0.16.19/cilium-windows-amd64.zip https://github.com/cilium/cilium-cli/releases/download/v0.16.19/cilium-windows-arm64.zip
+```
--- createManifest.ps1
+++ createManifest.ps1
@@ -7,6 +7,10 @@ if ($env:GITHUB_TOKEN) {
   }
 }
 
+# Initialize Git
+git config user.name 'github-actions[bot]'
+git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
+
 $packages = Get-ChildItem .\packages\*.json -Exclude schema.json
 foreach ($package in $packages) {
   $packageInfo = Get-Content -Raw $package | ConvertFrom-Json
@@ -15,23 +19,32 @@ foreach ($package in $packages) {
   $req = Invoke-RestMethod "https://api.github.com/repos/$($packageInfo.repoUrl)/releases/latest" -Headers $headers
   # Get Latest version number, remove `v` from string
   $latestVersion = $req.tag_name.Replace('v', '')
-  # Get download url for windows exe or zip files
-  $latestVersionDownloadURL = $req.assets | Where-Object name -Like '*windows*[zip|exe]' | Select-Object -ExpandProperty browser_download_url
+  # Once winget has support for tar.gz , add `|tar\.gz` to extension filter
+  $downloadURLFilter = $package.downloadURLFilter ? $package.downloadURLFilter : '.*windows.*\.(zip|exe)$'
+  # Get download url
+  $latestVersionDownloadURL = $req.assets | Where-Object name -Match $downloadURLFilter | Select-Object -ExpandProperty browser_download_url
 
   # Get contents of 'https://github.com/microsoft/winget-pkgs/tree/master/manifests/s/Sidero/talosctl'
   $wingetPackageList = Invoke-RestMethod "https://api.github.com/repos/microsoft/winget-pkgs/contents/$($packageInfo.wingetManifestPath)" -Headers $headers
-
-  # Get latest version of package on Winget 
-  $wingetLatestVersion = $wingetPackageList | Select-Object -Last 1 | Select-Object -ExpandProperty name
-
-  $packageIdentifier = $packageInfo.packageIdentifier
-  if (-not $packageIdentifier) {
-    # If no $packageIdentifier is defined in packages manifest, we construct one using the wingetManifestPath. Where the second last 
-    # subpath is org name and last bit of path is package name
-    $packageIdentifier = "$(($packageInfo.wingetManifestPath).split('/')[-2]).$(($packageInfo.wingetManifestPath).split('/')[-1])"
+  
+  # If the local manifest contains 'submittedVersion' we use that otherwise we get the latest version from winget
+  if($packageInfo.submittedVersion){
+    $wingetLatestVersion = $packageInfo.submittedVersion
+    Write-Host "Retrived latest version $($wingetLatestVersion) from local manifest."
   }
+  else {
+    $wingetPackageList | Select-Object -Last 1 | Select-Object -ExpandProperty name
+    Write-Host "submittedVersion not found in local manifest. Retreived $($wingetLatestVersion) from wingt."
+  }
+
   if ($latestVersion -ne $wingetLatestVersion) {
     Write-Host "Updating $($packageInfo.name) from '$wingetLatestVersion' to '$latestVersion'"
+    $packageIdentifier = $packageInfo.packageIdentifier
+    if (-not $packageIdentifier) {
+      # If no $packageIdentifier is defined in packages manifest, we construct one using the wingetManifestPath. Where the second last 
+      # subpath is org name and last bit of path is package name
+      $packageIdentifier = "$(($packageInfo.wingetManifestPath).split('/')[-2]).$(($packageInfo.wingetManifestPath).split('/')[-1])"
+    }
     $wingetCmd = ".\wingetcreate.exe update $packageIdentifier -s -v $latestVersion -u $($latestVersionDownloadURL -join (' '))" + ' -t $($env:WINGET_PAT)'
     Write-Host "Using cmd: $($wingetCmd)"
     # Download wingetcreate
@@ -39,7 +52,23 @@ foreach ($package in $packages) {
       Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile wingetcreate.exe
     }
     # Update the existing manifest
-    Invoke-Expression $wingetCmd -Verbose
+    Invoke-Expression $wingetCmd -Verbose -OutVariable wingetOutput
+    # First we get the lines that says 'Pull request can be found here:'
+    # Then split it at Spaces and get the last string, which is URL
+    $wingetPrURL = (($wingetOutput | Select-String -Pattern 'Pull request can be found here:\s+https:\/\/[\w\-\.\/]+').Matches[0] -split ' ')[-1]
+
+    Write-Host "PR URL: $wingetPrURL"
+
+    # If a PR is successfully submitted then update the local manifest 
+    if ($wingetPrURL) {
+      $packageInfo | Add-Member -MemberType NoteProperty -Name submittedVersion -Value $latestVersion
+      $packageInfo | Add-Member -MemberType NoteProperty -Name submittedPrURL -Value $wingetPrURL
+      $packageInfo | ConvertTo-Json | Set-Content -Path $package
+      git add $package
+      git commit -m "Updated $($packageInfo.name) to version $($latestVersion)"
+      git push
+    }
+
   }
   else {
     Write-Host "Latest version of $($packageInfo.name) ($($latestVersion))  is already present in Winget."
